675096a9e7ac1e5f5c8d48ad5d22b0fa61972a36
LOUIS SANNA <louissanna@gmail.com>
2021-08-28 23:21:52 +0200

    Install material-ui

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,19 @@
+import { makeStyles } from "@material-ui/core";
 import { TodoList } from './components';
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: '1200px',
+    margin: 'auto',
+    marginTop: '200px',
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <TodoList todos={[{ description: "Buy milk" }, { description: "Sell eggs" }]}/>
     </div>
   );
